4c11e067bb3e1bfa9f8cd449a0380ebf99501d25
parkgeonhu <parkgeonhu@gmail.com>
2018-09-19 06:56:00 +0000

    Add django-vote library

---
+++ test1/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+from vote.models import VoteModel
+
+class ArticleReview(VoteModel, models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+
+
